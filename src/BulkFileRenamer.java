dadc6a545694108b89b7acb945afab1847d621d6
ASH-CHA <ashton15chavez@gmail.com>
2026-08-20 03:53:16 +0200

    init

---
+++ src/BulkFileRenamer.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class BulkFileRenamer extends JFrame {
+
+    private File currentDir;
+    //private final List<FileRow> rows = new ArrayList<>();
+    //private final RowTableModel tableModel = new RowTableModel();
+    //private final JTable table = new JTable(tableModel);
+
+    public static void main(String[] args) {
+        
+    }
+}
